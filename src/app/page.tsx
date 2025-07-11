50963a46eb57a6346dba009c3c68dfe11b4195e4
Pratik Ghimire <147188683+pratik-gmre@users.noreply.github.com>
2025-07-11 11:37:52 +0545

    trpc-done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+
+
+import { useTRPC } from "@/trpc/client";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query";
 import Image from "next/image";
+import { Client } from "./client";
+import { Suspense } from "react";
 
 export default function Home() {
+  const query = getQueryClient()
+  void query.prefetchQuery(trpc.createAi.queryOptions({text: 'sairah'}))
+  
+
+    
   return (
- <div>Hello</div>
+<HydrationBoundary state={dehydrate(query)}>
+  <Suspense>
+    <Client/>
+  </Suspense>
+</HydrationBoundary>
   );
 }
